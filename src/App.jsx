a3db87acb121550bab5c6ee00f8e5404a8f6d7e5
Pravin <pravin@securly.com>
2026-01-06 13:22:41 +0530

    Web tinder react project files

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import Body from "./components/Body";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,9 @@
+const Profile = () => {
+  return (
+    <div className="profile-container flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">Profile Page</h1>
+    </div>
+  );
+};
+
+export default Profile;
